273aed24a21ea868143222bdc9cf29f4c3f6668d
Alessandro-Alino <48693504+Alessandro-Alino@users.noreply.github.com>
2024-03-18 10:48:42 +0100

    change list to SliverList

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d1c514c893ee8651a8b81e2e838b9b2c",
-"/": "d1c514c893ee8651a8b81e2e838b9b2c",
-"main.dart.js": "7636e88ce1d4e9162bbbfdbb87c72fc5",
+"index.html": "70a6998e9d0ad4ee2c5cc5f5489fa35c",
+"/": "70a6998e9d0ad4ee2c5cc5f5489fa35c",
+"main.dart.js": "47ec8631d0e32e50565740fe90d194cf",
 "manifest.json": "260e228b8b11ce544d876d55f95d7d0d",
 "version.json": "b7768ceafd67184f8c943dd0188a5639"};
 // The application shell files that are downloaded before a service worker can
